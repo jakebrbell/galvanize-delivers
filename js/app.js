65d75186a98006b614362beaf16e9e16c2aff6a5
Jake Bell <jakebrbell@gmail.com>
2016-05-23 07:56:21 -0700

    Clean up app.js file

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 (function() {
   'use strict';
   var $tbody = $('tbody');
-  var $target, menuItem, price, $tdMenuItem, $tdPrice, $tr;
-  var currentSubtotal, newSubtotal, tax, total;
-  var name, phone, address, checkName, checkPhone;
+  var address;
+  var menuItem;
+  var name;
+  var phone;
+  var price;
 
   // Initialize side navigation bar on mobile
   $('.button-collapse').sideNav();
@@ -11,10 +13,32 @@
   // Initialize parallax feature used on home page
   $('.parallax').parallax();
 
+  // Calculate subtotal, tax, and total on order form
+  var calculateTotals = function() {
+    var currentSubtotal;
+    var newSubtotal;
+    var tax;
+    var total;
+
+    price = Number(price.substr(1));
+    currentSubtotal = $('.subtotal').text();
+    currentSubtotal = Number(currentSubtotal.substr(1));
+    newSubtotal = currentSubtotal + price;
+    $('.subtotal').text('$' + newSubtotal);
+    tax = newSubtotal * 0.1;
+    $('.tax').text('$' + tax.toFixed(2));
+    total = newSubtotal + tax;
+    $('.total').text('$' + total.toFixed(2));
+  };
+
   // Add selected menu items to order
   $('.add-to-order').on('click', function(event) {
     event.preventDefault();
-    $target = $(event.target);
+    var $target = $(event.target);
+    var $tdMenuItem;
+    var $tdPrice;
+    var $tr;
+
     menuItem = $target.closest('.card').find('.card-title').text();
     price = $target.closest('.card').find('p').text();
     $tdMenuItem = $('<td>').attr('colspan', '2').text(menuItem);
@@ -25,26 +49,10 @@
     calculateTotals();
   });
 
-  // Calculate subtotal, tax, and total on order form
-  var calculateTotals = function() {
-    price = +price.substr(1);
-    currentSubtotal = $('.subtotal').text();
-    currentSubtotal = +currentSubtotal.substr(1);
-    newSubtotal = currentSubtotal + price;
-    $('.subtotal').text('$' + newSubtotal);
-    tax = (newSubtotal * 0.1);
-    $('.tax').text('$' + tax.toFixed(2));
-    total = newSubtotal + tax;
-    $('.total').text('$' + total.toFixed(2));
-  };
-
-  // Validate form data and submit form
-  $('.btn-large').on('click', function() {
-    name = $('#name').val();
-    phone = $('#phone').val();
-    address = $('#address').val();
-    checkName = /^[A-Za-z0-9 ]{3,30}$/;
-    checkPhone = /^(\+\d{1,2}\s)?1?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+  // Validate form
+  var submitOrder = function() {
+    var checkName = /^[A-Za-z0-9 ]{3,30}$/;
+    var checkPhone = /^(\+\d{1,2}\s)?1?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
 
     if ($tbody.children('tr').length === 0) {
       Materialize.toast('Please make a selection', 3000);
@@ -66,9 +74,17 @@
     }
     else {
       $tbody.children().remove();
-      $('.subtotal, .tax, .total').text('$0.00')
+      $('.subtotal, .tax, .total').text('$0.00');
       $('#name, #phone, #address').val('');
       Materialize.toast('Your order was successfully submitted', 3000);
     }
+  };
+
+  // Submit form
+  $('.btn-large').on('click', function() {
+    name = $('#name').val();
+    phone = $('#phone').val();
+    address = $('#address').val();
+    submitOrder();
   });
 })();
