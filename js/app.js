d8afce4298f4682126166b101c1f2e09a4c704ac
Jake Bell <jakebrbell@gmail.com>
2016-05-23 07:26:07 -0700

    Add images and copy for The Piebird

--- js/app.js
+++ js/app.js
@@ -1,37 +1,52 @@
 (function() {
   'use strict';
+  var $tbody = $('tbody');
+  var $target, menuItem, price, $tdMenuItem, $tdPrice, $tr;
+  var currentSubtotal, newSubtotal, tax, total;
+  var name, phone, address, checkName, checkPhone;
 
+  // Initialize side navigation bar on mobile
+  $('.button-collapse').sideNav();
+
+  // Initialize parallax feature used on home page
+  $('.parallax').parallax();
+
+  // Add selected menu items to order
   $('.add-to-order').on('click', function(event) {
     event.preventDefault();
-    var $target = $(event.target);
-    var menuItem = $target.closest('.card').find('.card-title').text();
-    var price = $target.closest('.card').find('p').text();
-    var $tdMenuItem = $('<td>').attr('colspan', '2').text(menuItem);
-    var $tdPrice = $('<td>').addClass('right-align price').text(price);
-    var $tr = $('<tr>').append($tdMenuItem).append($tdPrice);
-    $('tbody').append($tr);
+    $target = $(event.target);
+    menuItem = $target.closest('.card').find('.card-title').text();
+    price = $target.closest('.card').find('p').text();
+    $tdMenuItem = $('<td>').attr('colspan', '2').text(menuItem);
+    $tdPrice = $('<td>').addClass('right-align price').text(price);
+    $tr = $('<tr>').append($tdMenuItem).append($tdPrice);
+    $tbody.append($tr);
 
-    price = +price.substr(1);
-    var currentSubtotal = $('.subtotal').text();
-    currentSubtotal = +currentSubtotal.substr(1);
-    var newSubtotal = currentSubtotal + price;
-    $('.subtotal').text('$' + newSubtotal);
-
-    var tax = (newSubtotal * 0.1);
-    $('.tax').text('$' + tax.toFixed(2));
-
-    var total = newSubtotal + tax;
-    $('.total').text('$' + total.toFixed(2));
+    calculateTotals();
   });
 
-  $('.btn-large').on('click', function() {
-    var name = $('#name').val();
-    var phone = $('#phone').val();
-    var address = $('#address').val();
-    var checkName = /^[A-Za-z0-9 ]{3,30}$/;
-    var checkPhone = /^(\+\d{1,2}\s)?1?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+  // Calculate subtotal, tax, and total on order form
+  var calculateTotals = function() {
+    price = +price.substr(1);
+    currentSubtotal = $('.subtotal').text();
+    currentSubtotal = +currentSubtotal.substr(1);
+    newSubtotal = currentSubtotal + price;
+    $('.subtotal').text('$' + newSubtotal);
+    tax = (newSubtotal * 0.1);
+    $('.tax').text('$' + tax.toFixed(2));
+    total = newSubtotal + tax;
+    $('.total').text('$' + total.toFixed(2));
+  };
 
-    if ($('tbody').children('tr').length === 0) {
+  // Validate form data and submit form
+  $('.btn-large').on('click', function() {
+    name = $('#name').val();
+    phone = $('#phone').val();
+    address = $('#address').val();
+    checkName = /^[A-Za-z0-9 ]{3,30}$/;
+    checkPhone = /^(\+\d{1,2}\s)?1?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+
+    if ($tbody.children('tr').length === 0) {
       Materialize.toast('Please make a selection', 3000);
     }
     else if (name === '') {
@@ -50,11 +65,10 @@
       Materialize.toast('Please enter an address', 3000);
     }
     else {
-      $('tbody').children().remove();
+      $tbody.children().remove();
       $('.subtotal, .tax, .total').text('$0.00')
       $('#name, #phone, #address').val('');
       Materialize.toast('Your order was successfully submitted', 3000);
     }
   });
-
 })();
